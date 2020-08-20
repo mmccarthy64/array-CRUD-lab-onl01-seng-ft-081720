70d57ac06a033d5fbced9a46ca70147f3a08740c
Matthew McCarthy <mmccarthy64@hotmail.com>
2020-08-20 18:21:29 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -15,12 +15,13 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  create_an_array.pop
+   x = ["wow",2,3,"arrays!"]
+  x.pop()
 end
 
 def remove_element_from_start_of_array(array)
   x = ["wow",2,3,"arrays!"]
-  x.pop()
+  x.shift()
 end
 
 def retrieve_element_from_index(array, index_number)
